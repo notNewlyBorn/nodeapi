3c0a75efe89153b3bf3cf65b2771f30f4f00df4b
nafiskarim <sahil.abrar@gmail.com>
2017-07-11 18:56:05 +0200

    test(showing): test added, works fine
    create a test with chai and supertest for user listing api call

--- test/app.test.js
+++ test/app.test.js
@@ -1,15 +1,32 @@
+const request = require('supertest')
+const expect = require('chai').expect
 const knex = require('../db/knex') // connection
 
+const app = require('../app')
+
+const fixtures = require('./fixtures') // importing a test data to compare with the result
+
 describe('CRUD users',() => {
-  before(() => {
+  before((done) => {
     // run migration
-    knex.migrate.lates()
-    .then(() => {
-      return knex.seed.run()
-    })
+    knex.migrate.latest()
     .then(() => {
       // run seeds
       return knex.seed.run()
-    })
+    }).then(() => done())
   })
+
+  it('Lists all records', function(done) {
+    request(app)
+      .get('/api/v1/users')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).to.be.a('array')
+        console.log(response.body);
+        expect(response.body).to.deep.equal(fixtures.users)
+        done()
+      })
+  });
 })
---
+++ test/fixtures.js
@@ -0,0 +1,10 @@
+const users = [
+  { id: 1, name: 'arla', point: 10 },
+  { id: 2, name: 'karim', point: 20 },
+  { id: 3, name: 'nafis', point: 30 },
+  { id: 4, name: 'deff', point: 10 },
+  { id: 5, name: 'merry', point: 5 }
+]
+module.exports = {
+  users
+}
