0fd8d91902e664f0b267131e4d026fb68e8ea71d
nafiskarim <sahil.abrar@gmail.com>
2017-07-11 16:11:06 +0200

    feat(database): create knexfile, create user table migration and seed user table

---
+++ seeds/01_user.js
@@ -0,0 +1,10 @@
+var users = require('../users')
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('user').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('user').insert(users);
+    });
+};
---
+++ users.js
@@ -0,0 +1,23 @@
+
+module.exports = [
+  {
+    name: "arla",
+    point: 10,
+  },
+  {
+    name: "karim",
+    point: 20,
+  },
+  {
+    name: "nafis",
+    point: 30,
+  },
+  {
+    name: "deff",
+    point: 10,
+  },
+  {
+    name: "merry",
+    point: 5,
+  },
+]
