94563dfc6d3747fa46e6c7c3e411e2ba8dc3a7c1
nafiskarim <sahil.abrar@gmail.com>
2017-07-11 16:37:11 +0200

    refactor(refactor): removing everything related to view layer
    remove view and static serve, update error handler

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var users = require('./routes/users');
-
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
-app.use('/api/v1/users', users);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  });
+});
 
 module.exports = app;
