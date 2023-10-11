877c9200a7a9b8d5e6c428b1304d27ac85dc55d8
Nafis Karim <nafis_20@yahoo.com>
2023-10-11 17:20:35 +0200

    another change in app.js on master

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const app = express();
 
-const usersRouter = require('./api/users')
+const usersRouter = require("./api/users");
 
 // middlewears
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use('/api/v1/users', usersRouter)
+app.use("/api/v1/users", usersRouter);
 
-// catch 404 and forward to error handler. some dummy text
+// catch 404 and forward to error handler. some dummy text. and more
 app.use(function (req, res, next) {
-  var err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     message: err.message,
-    error: req.app.get('env') === 'development' ? err : {}
+    error: req.app.get("env") === "development" ? err : {},
   });
 });
 
